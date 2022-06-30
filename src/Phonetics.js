771aa2526093c575762552d42a6eb847a58df5d5
TheSmilycake <wiedaurebecca@gmail.com>
2022-06-30 12:59:59 +0200

    Add key to phonetics

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -6,10 +6,10 @@ export default function Phonetics(props) {
         <ul>
         {props.phonetics.map((phonetic, index) => {
             if (phonetic.audio === "") {
-                return <li className="Phonetic"><span>{phonetic.text}</span></li>
+                return <li key={index} className="Phonetic"><span>{phonetic.text}</span></li>
             }
             return (
-                <li className="Phonetic"><span>{phonetic.text}</span><a className="audio" href={phonetic.audio} title={phonetic.audio}>[Listen]</a></li>)
+                <li className="Phonetic" key={index}><span>{phonetic.text}</span><a className="audio" href={phonetic.audio} title={phonetic.audio}>[Listen]</a></li>)
         })}
     </ul></div>;
 }
